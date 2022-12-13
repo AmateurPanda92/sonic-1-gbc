32c59a6b7e9cb59dae4aaca1058c16a881e90073
Andi Emma Davies <andi@stackinabox.co.uk>
2022-12-13 19:15:37 +0000

    Call the asset server from the sprite viewer (PoC)

--- Tools/AssetServer/index.ts
+++ Tools/AssetServer/index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import fs from "fs/promises";
@@ -7,20 +8,14 @@ import "dotenv-defaults/config";
 dotenv.config();
 
 const app = express();
+
 const port = process.env["PORT"];
+const clientHost = process.env["CLIENT_HOST"];
+
+app.use(cors({ origin: clientHost }));
 
 app.listen(port, () => console.log(`[server]: Server is running at http://localhost:${port}/.`));
 
-/* TODO:
- * - [ ] Read the relative location of the assets directory from config.
- * - [x] Introduce a default config values file.
- * - [ ] Create a filesystem access shim around fs package calls.
- * - [ ] Change the functionality to be reusable and dynamic, instead of hardcoded for specific assets.
- * - [ ] Create a TS data model that can be leveraged to validate assets against the schema (ideally as its own project that can be consumed and updated via npm).
- * - [ ] Ensure the data is sent back with the correct MIME type.
- * - [ ] Change to be a general "data" server, instead of just assets (i.e. rename the app).
- * - [ ] Make it so that all tools can be started from one command and integrate seamlessly (will likely need to publish each tool via npm so that it can be consumed by the dependent tools).
- */
 app.get("/graphics/sprites/sonic/idle", async (_request, response) => {
     const location = "../../Data/Assets/Graphics/Sprites/Sonic/Idle.json";
     const buffer = await fs.readFile(location);
--- Tools/SpriteViewer/src/App.tsx
+++ Tools/SpriteViewer/src/App.tsx
@@ -1,11 +1,31 @@
 import Heading from "Components/Heading";
 import Paragraph from "Components/Paragraph";
 
-const App = () => (
-    <>
-        <Heading level={1} emoji="🔎" copy="AmateurPanda92 Sprite Viewer" />
-        <Paragraph copy="Hello, spritey world!" />
-    </>
-);
+import { useCallback, useEffect, useState } from "react";
+
+const assetServerHost = process.env["REACT_APP_ASSET_SERVER_HOST"];
+const idleSonicSpritePath = "/graphics/sprites/sonic/idle";
+
+const App = () => {
+    const [json, setJson] = useState<string>();
+
+    const fetchJson = useCallback(async () => {
+        const response = await fetch(`${assetServerHost}${idleSonicSpritePath}`);
+        const json = await response.json();
+        const serialised = JSON.stringify(json);
+
+        setJson(serialised);
+    }, []);
+
+    useEffect(() => { fetchJson(); }, [fetchJson]);
+
+    return (
+        <>
+            <Heading level={1} emoji="🔎" copy="AmateurPanda92 Sprite Viewer" />
+            <Paragraph copy="Hello, spritey world!" />
+            <code>{json}</code>
+        </>
+    );
+};
 
 export default App;
